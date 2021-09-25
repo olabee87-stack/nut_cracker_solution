2e43d64163d9521b1e94f193d82f58b9ac8afe30
Olabisi Odusanya <odus.bisi07@gmail.com>
2021-09-25 17:30:58 +0300

    Feat: Function to post the result of the binary tree

--- src/controllers/nut-cracker.js
+++ src/controllers/nut-cracker.js
@@ -1,10 +1,10 @@
-import getTestApi from "../helper/get-test-api.js";
+import { getTestApi, postToTestApi } from "../helper/test-api-helpers.js";
+import { binarySearch } from "../helper/binary-search-helper.js";
 
 // @GET - get data from the API
 export const getData = async (req, res) => {
   try {
     res.status(200).json(await getTestApi());
-    console.log("Gotten");
   } catch (err) {
     return res
       .status(400)
@@ -12,11 +12,57 @@ export const getData = async (req, res) => {
   }
 };
 
-// @POST - Post to vello server
-export const postData = async (req, res) => {
+// @POST result ('/')
+export const postResultOfBinaryTree = async (req, res) => {
+  // Get the weight data out of the api
+  let data = await getTestApi();
+  let target = data.key;
+  const getWeight = data.nodes.map((wei) => {
+    const { weight } = wei;
+    return weight;
+  });
+
+  // Check if weight was returned
+  if (!getWeight) {
+    return res.send(`No weights found, check the data again`);
+  }
+
+  // Make a callback function with your binarySearch
+  // Send the result of your check as a post request
+
   try {
-    await res.status(201).json({ Message: "Post sent!" });
-  } catch (error) {}
+    res
+      .status(200)
+      .json(
+        await binarySearch(getWeight, target, async () => await postToTestApi())
+      );
+    console.log(
+      await binarySearch(getWeight, target, async () => await postToTestApi())
+    );
+  } catch (error) {
+    return res
+      .status(400)
+      .json({ Message: `Unable to get data::: ${error.message}` });
+  }
 };
 
 // Ship off to routes - nut-cracker.js
+
+// Test Sample - with mini-array
+export const samplePost = async (req, res) => {
+  let myTestNodes = [1, 7, 8, 5, 3, 5, 50, 687];
+  let target = 50;
+
+  try {
+    res
+      .status(200)
+      .json(
+        binarySearch(myTestNodes, target, async () => await postToTestApi())
+      );
+    console.log(await postToTestApi());
+  } catch (error) {
+    return res
+      .status(400)
+      .json({ Message: `Unable to get data ${error.message}` });
+  }
+};
